cfa3a007d20d76433240b981fa2b01c695f391cc
Saurav576 <sauravkumarmishra576@gmail.com>
2025-07-19 13:21:29 +0530

    commit

--- servers/controllers/orderController.js
+++ servers/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder=async(req,res)=>{
 
-    const frontend_url="https://food-delivery2-frontend.onrender.com";
+    const frontend_url="https://food-delivery2-frontend-5cdy.onrender.com/";
 
     try {
         const newOrder=new orderModel({
