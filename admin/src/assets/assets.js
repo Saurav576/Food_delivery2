19d26bcaefdbe4c01365fb5d42dcebd12a440c88
Saurav576 <sauravkumarmishra576@gmail.com>
2025-07-19 13:02:38 +0530

    commit

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-deliverybackend-lws6.onrender.com'
+export const url = 'https://food-delivery2-backend.onrender.com'
--- frontend_panel/src/context/StoreContext.jsx
+++ frontend_panel/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems]=useState({});
-    const url="https://food-deliverybackend-lws6.onrender.com"
+    const url="https://food-delivery2-backend.onrender.com"
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([])
 
